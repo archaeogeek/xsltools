d73c1a7823b70696118d8d7ca79d8d2fc982c1de
jo <jo@alien>
2014-11-01 17:10:38 +0000

    Deleted files that are not needed

--- importphotos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Wait a while to allow the media to be mounted if
-# we are being called from a udev rule
-sleep 10
-
-# Simply copy the files over to the Camera Uploads dir where they will be
-# automagically imported from
-cp /media/5508-D13C/DCIM/100_PANA/* "/home/kate/Dropbox/Camera Uploads/"
